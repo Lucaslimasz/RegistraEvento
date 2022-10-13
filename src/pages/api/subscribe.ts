62b70b0bae929cb4d68a39ab730823fd72a8c657
Lucas Lima <id.lucas@outlook.com.br>
2022-10-12 21:48:46 -0300

    failed connection faunadb

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,46 @@
+import { fauna } from "@config/fauna";
 import { stripe } from "@config/stripe";
+import { query as q } from 'faunadb';
 import { NextApiRequest, NextApiResponse } from "next";
+
+interface User {
+  ref: {
+    id: string;
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {email} = req.body
   if(req.method === 'POST'){
+
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(email)
+        )
+      )
+    ) 
     
     const stripeCustomer: any = await stripe.customers.create({
       email
     })
 
+    await fauna.query(
+      q.Update(
+        q.Ref(q.Collection('users'), user.ref.id),
+        {
+          data: { 
+            stripe_customer_id: stripeCustomer.id,
+          }
+        }
+      )
+    )
+
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: stripeCustomer.id,
       payment_method_types: ['card'],
-      billing_address_collection: 'required',
+      billing_address_collection: 'auto',
       line_items: [
         {price: 'price_1Lq0gyBGniSjo37yWKVX4VFc', quantity: 1}
       ],
