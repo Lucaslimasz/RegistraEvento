239394cac780d93184c9017b45b07f2a7b027d5a
Lucas Lima <id.lucas@outlook.com.br>
2023-04-16 15:30:42 -0300

    configuration

--- src/components/CardSubscribe/index.tsx
+++ src/components/CardSubscribe/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import * as S from "./styles";
 
-import { conference } from "../../../event.json";
+import conference from "../../../event.json";
 
 interface IUser {
   user: {
@@ -14,19 +14,19 @@ interface IUser {
   };
 }
 
-const CardSubscribe = ({ user }: IUser) => {
+const CardSubscribe = ({ user }: IUser & any) => {
   return (
     <S.Container>
       <img src="/assets/card-subscribe.png" alt="subscribe" />
-      <h1>{conference[0].title}</h1>
+      <h1>{conference.conference[0].title}</h1>
       <ul>
         <li>
           <img src="/assets/icons/calendar.svg" alt="calendar" />
-          <b>{conference[0].date}</b>
+          <b>{conference.conference[0].date}</b>
         </li>
         <li>
           <img src="/assets/icons/location.svg" alt="location" />
-          <b>{conference[0].location}</b>
+          <b>{conference.conference[0].location}</b>
         </li>
         <span>#{user?._id?.substring(0, 7)}</span>
       </ul>
